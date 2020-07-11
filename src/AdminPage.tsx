691e1e6cf9983913f4b2f60b8709c6228a6a2f70
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-07-12 00:34:40 +0100

    finished the react shop v1

---
+++ src/AdminPage.tsx
@@ -0,0 +1,87 @@
+import * as React from "react"
+import {NavLink,Route,RouteComponentProps} from "react-router-dom"
+
+
+const AdminPage:React.SFC =() => {
+    return(
+        <div className="page-container">
+            <h1>Admin Panel</h1>
+            <ul className="admin-section">
+                <li key="users">
+                    <NavLink to={`/admin/users`}
+                     activeClassName="admin-link-active" >
+                         Users
+                    </NavLink>
+                </li>
+                <li key="products">
+                    <NavLink to={`/admin/products`}
+                     activeClassName="admin-link-active" >
+                         Products
+                    </NavLink>
+                </li>
+            </ul>
+            <Route path="/admin/users" component={AdminUsers} />
+            <Route exact path="/admin/products" component={AdminProducts} />
+        </div>
+    )
+}
+
+const AdminProducts:React.SFC = () => {
+    return <div>Some Options to administer products</div>
+}
+
+interface IUser {
+    id:number;
+    name:string;
+    isAdmin:boolean
+}
+const adminUsersData:IUser[] = [
+    {id:1,name:"Fred",isAdmin:true},
+    {id:2,name:"Bob",isAdmin:true},
+    {id:3,name:"Jane",isAdmin:true}
+]
+
+const AdminUsers:React.SFC = () => {
+    return(
+        <div>
+            <ul className="admin-sections">
+                {
+                    adminUsersData.map(user => (
+                        <li key={user.id}>
+                            <NavLink 
+                            to={`/admin/users/${user.id}`}>
+                                {user.name}
+                            </NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+            <Route exact path="/admin/users/:id" component={AdminUser} />
+        </div>
+    )
+}
+
+const AdminUser:React.SFC<RouteComponentProps<{id:string}>> =
+ (props) => {
+    let user:IUser;
+    if(props.match.params.id){
+        const id:number = parseInt(props.match.params.id,10);
+        user = adminUsersData.filter(u => u.id === id)[0]
+    }else{
+        return null
+    }
+    return(
+        <div>
+            <div>
+                <b>Id:</b>
+                <span>{user.id.toString()}</span>
+            </div>
+            <div>
+                <b>Is Admin:</b>
+                <span>{user.isAdmin.toString()}</span>
+            </div>
+        </div>
+    )
+}
+
+export default AdminPage;
---
+++ src/Routes.tsx
@@ -0,0 +1,71 @@
+import React,{Suspense} from "react";
+import {BrowserRouter as Router,RouteComponentProps,
+    Switch,Route,Redirect} from "react-router-dom"
+import {CSSTransition,TransitionGroup} from "react-transition-group"
+import Header from "./Header"
+import ProductsPage from './ProductsPage'
+import ProductPage from "./ProductPage"
+import LoginPage from "./LoginPage"
+import "./index"
+const AdminPage = React.lazy(() => import ("./AdminPage"))
+
+
+interface IState {
+    loggedIn:boolean
+}
+
+const RoutesWrap:React.SFC = () => {
+    return(
+        <Router>
+            <Route component={Routes}/>
+        </Router>
+    )
+}
+
+    class Routes extends React.Component<RouteComponentProps,IState>{
+        constructor(props:RouteComponentProps){
+            super(props)
+            this.state = {
+                loggedIn:true
+            }
+        }
+
+        render(){
+            console.log(this.props)
+            return(
+                <div>
+                    <Header/>
+                    <TransitionGroup>
+                        <CSSTransition
+                          timeout={500}
+                          classNames="animate"
+                          key={this.props.location.key}>
+                        <Switch>
+                        <Route exact path="/products"
+                         component={ProductsPage}/>
+                        <Route path="/admin"
+                         component={AdminPage}>
+                            {this.state.loggedIn ? 
+                            (<Suspense fallback={
+                                <div className="page-loading">Loading...</div>
+                            }>
+                                <AdminPage/>
+                            </Suspense>) : 
+                            (<Redirect to="/login" />)}
+                        </Route>
+                        <Route path="/login" component={LoginPage} />
+                        <Route path="/products/:id"
+                         component={ProductPage} />
+                        <Route render={() => (<Redirect to="/products" />)} />
+                         </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                    </div>
+            )
+        }
+    }
+//     const [loggedIn,setLoggedIn] = React.useState(true)
+    
+// }
+
+export default RoutesWrap
