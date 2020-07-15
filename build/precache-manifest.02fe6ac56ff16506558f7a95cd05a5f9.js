f9de9cc9c652dd4d78d056663afc5647aeecb2f4
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-07-15 01:28:12 +0100

    added the build

---
+++ build/precache-manifest.02fe6ac56ff16506558f7a95cd05a5f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd93427635681c2d56745d72a377142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33315ee0a7cd34fbc044",
+    "url": "/static/css/main.a0ba5596.chunk.css"
+  },
+  {
+    "revision": "6030befaddc246665f85",
+    "url": "/static/js/2.44a80445.chunk.js"
+  },
+  {
+    "revision": "0a2c9cd3cc2b6b30e1609433a4b228af",
+    "url": "/static/js/2.44a80445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbc05e1e6cc6cca80aa",
+    "url": "/static/js/3.6bf67438.chunk.js"
+  },
+  {
+    "revision": "33315ee0a7cd34fbc044",
+    "url": "/static/js/main.cbb9bf3e.chunk.js"
+  },
+  {
+    "revision": "8afd3835910df6517111",
+    "url": "/static/js/runtime-main.d788c1d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
